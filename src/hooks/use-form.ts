7fbc24f01e31d8a346b86b2f7e91cdd67633b0ea
João Fernando do Amaral Brigido <70164946+joaofbrigido@users.noreply.github.com>
2024-12-17 20:08:03 -0300

    use-form aceitar parametros

--- src/hooks/use-form.ts
+++ src/hooks/use-form.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FormEvent, useState, useTransition } from "react";
-// import { requestFormReset } from "react-dom";
 
 interface FormState {
   success: boolean;
@@ -8,10 +8,9 @@ interface FormState {
 }
 
 export function useForm(
-  action: (data: FormData) => Promise<FormState>,
+  action: (data: FormData, extraData?: any) => Promise<FormState>,
   initialState?: FormState,
   onSuccess?: () => Promise<void> | void
-  // resetForm?: boolean
 ) {
   const [isPending, startTransition] = useTransition();
 
@@ -19,14 +18,17 @@ export function useForm(
     initialState ?? { success: false, message: null, errors: null }
   );
 
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  async function handleSubmit(
+    event: FormEvent<HTMLFormElement>,
+    extraData?: any
+  ) {
     event.preventDefault();
 
     const form = event.currentTarget;
     const data = new FormData(form);
 
     startTransition(async () => {
-      const state = await action(data);
+      const state = await action(data, extraData);
 
       if (state.success && onSuccess) {
         await onSuccess();
@@ -34,8 +36,6 @@ export function useForm(
 
       setFormState(state);
     });
-
-    // if (resetForm) requestFormReset(form);
   }
 
   return [formState, handleSubmit, isPending] as const;
